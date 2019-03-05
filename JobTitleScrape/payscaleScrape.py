f7d7a3c1214ee9b4e8aede45df8e4927b41fa7dd
Eric Banta <44131634+ericgbanta@users.noreply.github.com>
2019-03-05 11:42:24 -0800

    added sql create file and itterrows to sql table

--- JobTitleScrape/payscaleScrape.py
+++ JobTitleScrape/payscaleScrape.py
@@ -1,7 +1,7 @@
-# %% Import
+import pyodbc
+import pandas as pd
 import requests
 from selenium import webdriver
-import pandas as pd
 import numpy as np
 import datetime
 from string import ascii_uppercase
@@ -10,18 +10,24 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-# %% BLOCK 1 - See website %% #
+# %% BLOCK 0.1 - See website - OPTIONAL %% #
 url = 'https://www.payscale.com/index/CA/Job/'
 driver = webdriver.Chrome(
-    executable_path=r'C:\Users\ericg\Downloads\chromedriver_win32\chromedriver.exe')
+    executable_path=r'C:\Users\eric.banta\Downloads\chromedriver_win32\chromedriver.exe')
 driver.get(url)
 
 
-# %% BLOCK 2 - Scrape Tables for titles - OPTIONAL %% #
+# %% Block 1 - Connect and run query
+cnxn = pyodbc.connect('DRIVER={SQL Server};'
+                      'SERVER=ERICLOCAL;'
+                      'DATABASE=Payscale;'
+                      'trusted_connection=yes;')
+
+
+# %% BLOCK 2 - Scrape Tables for titles %% #
 # Create 'Emoty DataFrame'
 todays_date = datetime.datetime.now().date()
-index = pd.date_range(
-    todays_date - datetime.timedelta(10), periods=10, freq='D')
+index = pd.date_range(todays_date - datetime.timedelta(10), periods=10, freq='D')
 columns = ['A']
 
 finalDF = []
@@ -43,6 +49,12 @@ finalDF = finalDF.reset_index()
 finalDF = finalDF.drop('index', 1)
 finalDF
 
+# rename and reorder columns
+cols = finalDF.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+finalDF = finalDF[cols]
+finalDF
+
 
 # %% BLOCK 3 - For each title, pull the link associated with each %% #
 url = 'https://www.payscale.com'
@@ -57,17 +69,34 @@ for letter in ascii_uppercase:
 urls = [url + link for link in urls]
 
 print(urls)
-print(len(urls))  # check if amount of links = amount of titles
+print(len(urls))
 
 
-# %% BLOCK 4 - Append urls to dataframe %% #
+# %% BLOCK 4 - Append urls to dataframe and add datetime column %% #
 urlArray = np.asarray(urls)
 data = finalDF.copy()
 data['URLS'] = urlArray
+data['Updated Date'] = pd.to_datetime('today')
+data['PayscaleId'] = range(1, 1 + len(data))
+cols = data.columns.tolist()
+cols = cols[-1:] + cols[:-1]
+data = data[cols]
 data
 
 
-# %% BLOCK 5 - Export to excel file
-writer = pd.ExcelWriter('PayscaleExport.xlsx')
-data.to_excel(writer, 'Sheet 1', index=None)
-writer.save()
+# %% BLOCK 5 - Dataframe to SQL
+cursor = cnxn.cursor()
+
+# delete table data
+cursor.execute('TRUNCATE TABLE eric.payscaletest')
+
+
+# insert new table data
+qry = '''INSERT INTO eric.payscaletest([PayscaleId], [Popular Jobs], [Number of Data Profiles], [URL], [Updated Date]) values (?, ?, ?, ?, ?)'''
+
+for index, row in data.iterrows():
+    cursor.execute(qry, *row)
+    cnxn.commit()
+
+cursor.close()
+cnxn.close()
---
+++ JobTitleScrape/payscale_create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [eric].[payscaletest]
+(
+	[PayscaleId] [int] NOT NULL PRIMARY KEY,
+	[Popular Jobs] [varchar](150) NOT NULL,
+	[Number of Data Profiles] [float] NOT NULL,
+	[URL] [varchar](max) NOT NULL,
+	[Updated Date] [datetime] NOT NULL
+)
